2d0b1ca1baa4e2ee54a155fbdc7b8526ad56bdae
starburst3190 <jackuang0008@gmail.com>
2026-09-22 09:58:31 +0800

    Initial Commit

---
+++ Shapes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shapes
+//
+//  Created by Jack on 2026/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Jack on 2026/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
